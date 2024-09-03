5ced7fcb4897669432a88183ff6543d4f6727665
MrAndiScripts <78992535+MrAndiGamesDev@users.noreply.github.com>
2024-09-03 13:57:38 -0400

    Updated Buttons and UI

--- realisicwater.js
+++ realisicwater.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
+function realisicwater(){
     $(".full-landing-image").ripples({
         resolution: 200,
         dropRadius: 50,
-        perturbance: .004,
+        perturbance: .002,
     });
-});
+}
+
+$(document).ready(realisicwater());
