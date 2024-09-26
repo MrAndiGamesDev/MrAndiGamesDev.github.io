877cb9c5b08e79a976ee2ac81f003dcea96b15bf
MrAndiScripts <78992535+MrAndiGamesDev@users.noreply.github.com>
2024-09-26 14:10:42 -0400

    Delete assets directory

--- assets/scripts/realisicwater.js
+++
@@ -1,9 +0,0 @@
-function realisicwater(){
-    $(".full-landing-image").ripples({
-        resolution: 200,
-        dropRadius: 50,
-        perturbance: .002,
-    });
-}
-
-$(document).ready(realisicwater());
